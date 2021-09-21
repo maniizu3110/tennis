6b591ea3e37479fe433e5f57ce417e46569c178e
Leo <49385552+maniizu3110@users.noreply.github.com>
2021-09-21 15:16:02 +0900

    next.jsの作成

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/global.css'
 
-export default function App({ Component, pageProps }) {
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
