9c5c8526144f9086add22f0b0663cfdc39190cec
maniizu3110 <maniizu3110@gmail.com>
2021-09-29 10:49:49 +0900

    userを取得するエンドポイントの作成

--- server/api/handler/user_handler.go
+++ server/api/handler/user_handler.go
@@ -2,43 +2,51 @@ package handler
 
 import (
 	"net/http"
+	"server/api/service"
 	"server/api/sqlc"
 
 	"github.com/labstack/echo/v4"
 	"github.com/sirupsen/logrus"
 )
 
-func AssignUserHandler(g *echo.Group){
-	g = g.Group("",func(handler echo.HandlerFunc) echo.HandlerFunc{
-		return func(c echo.Context)error{
+func AssignUserHandler(g *echo.Group) {
+	g = g.Group("", func(handler echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
 			store := c.Get("store").(sqlc.Store)
-			s := service.NewUserStore(store)
-			c.Set("Service",s)
+			s := service.NewUserService(store)
+
+			c.Set("Service", s)
 			return handler(c)
 		}
 	})
-	g.GET("/:id", GetUserHandler)
-	g.GET("/", GetAllUserHandler)
+	// g.GET("/:id", GetUserHandler)
+	g.GET("/", ListUserHandler)
 }
 
-func GetUserHandler(c echo.Context) error {
+// func GetUserHandler(c echo.Context) error {
+// 	service := c.Get("Service").(service.UserService)
+// 	type getUserParams struct {
+// 		Username string
+// 	}
+// 	params := &getUserParams{}
+// 	c.Bind(params)
+// 	User, err := service.GetUser(params.Username)
+// 	if err != nil {
+// 		logrus.Info(err.Error())
+// 		return err
+// 	}
+// 	return c.JSON(http.StatusOK, User)
+// }
+
+func ListUserHandler(c echo.Context) error {
 	service := c.Get("Service").(service.UserService)
-	type getUserParams struct {
-		Username string
-	}
-	params := &getUserParams{}
+	//TODO:offsetとlimitをoptionにする
+	params := sqlc.ListUserParams{}
 	c.Bind(params)
-	User, err := service.GetUser(params.Username)
-	if err != nil {
-		logrus.Info(err.Error())
-		return err
+	if params.Limit == 0 {
+		params.Limit = 10
 	}
-	return c.JSON(http.StatusOK, User)
-}
-
-func GetAllUserHandler(c echo.Context) error{
-	service := c.Get("Service").(service.UserService)
-	users, err := service.GetAllUser()
+	users, err := service.ListUser(params)
 	if err != nil {
 		logrus.Info(err.Error())
 		return err
--- server/api/service/user_service.go
+++ server/api/service/user_service.go
@@ -1,6 +1,23 @@
 package service
 
-import "server/api/sqlc"
+import (
+	"context"
+	"server/api/sqlc"
+)
+
+type UserService interface {
+	ListUser(params sqlc.ListUserParams) ([]sqlc.User, error)
+}
+
+type userServiceImpl struct {
+	store sqlc.Store
+}
+
+func NewUserService(store sqlc.Store) UserService {
+	res := &userServiceImpl{}
+	res.store = store
+	return res
+}
 
 type userResponse struct {
 	ID    int64  `json:"id"`
@@ -10,8 +27,17 @@ type userResponse struct {
 
 func newUserResponse(user sqlc.User) *userResponse {
 	return &userResponse{
-		ID:        user.ID,
-		Name:      user.Name,
-		Email:     user.Email,
+		ID:    user.ID,
+		Name:  user.Name,
+		Email: user.Email,
 	}
+}
+
+func (s *userServiceImpl) ListUser(params sqlc.ListUserParams) ([]sqlc.User, error) {
+	listUser, err := s.store.ListUser(context.Background(), params)
+	if err != nil {
+		return nil, err
+	}
+	return listUser, nil
+
 }
